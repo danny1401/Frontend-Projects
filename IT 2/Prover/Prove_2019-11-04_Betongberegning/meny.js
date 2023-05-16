04685ef86caf9b0a260cf24d270c7390578503a2
Danny Thinh Tran <72601388+danny1401@users.noreply.github.com>
2023-05-16 13:59:37 +0200

    Add files via upload

---
+++ IT 2/Prover/Prove_2019-11-04_Betongberegning/meny.js
@@ -0,0 +1,21 @@
+
+// Meny legges inn i variabler. Det kunne være en variabel, 
+// men linjeskift avslutter javascript-setninger så for 
+// oversiktlighet er den delt på data_a til data_x.
+var data_b = '<div id="bar"></div>';
+var data_c = '<h1 id="navtitle">Betongberegning</h1>';
+var data_d = '<a href="index.html" class="navbtn" id="navbtn1">Home</a>';
+var data_e = '<a href="./index.html#info" class="navbtn" id="navbtn2">Info</a>';
+var data_f = '<a href="./index.html#bilder" class="navbtn" id="navbtn3">Bilder</a>';
+var data_g = '<a href="./beregn.html" class="navbtn" id="navbtn4">Beregn</a>';
+var data_x = '</div>';
+
+
+//Her er utskrift av variablene.
+document.write(data_b);
+document.write(data_c);
+document.write(data_d);
+document.write(data_e);
+document.write(data_f);
+document.write(data_g);
+document.write(data_x);
---
+++ IT 2/Prover/Prove_2019-12-09_Oppvarming/meny.js
@@ -0,0 +1,23 @@
+
+// Meny legges inn i variabler. Det kunne være en variabel, 
+// men linjeskift avslutter javascript-setninger så for 
+// oversiktlighet er den delt på data_a til data_x.
+var data_a = '<div id="bar"></div>';
+var data_b = '<h1 id="navtitle">Oppvarming<strong> </strong>av<strong> </strong>hus</h1>';
+var data_c = '<a href="index.html" class="navbtn" id="navbtn1">Hjem</a>';
+var data_d = '<a href="./volum.html" class="navbtn" id="navbtn2">Volum</a>';
+var data_e = '<a href="./oppvarming.html" class="navbtn" id="navbtn3">Oppvarming</a>';
+var data_f = '<a href="./ventilasjon.html" class="navbtn" id="navbtn4">Ventilasjon</a>';
+var data_g = '<a href="./varmebehov.html" class="navbtn" id="navbtn5">Varmebehov</a>';
+var data_x = '</div>';
+
+
+//Her er utskrift av variablene.
+document.write(data_a);
+document.write(data_b);
+document.write(data_c);
+document.write(data_d);
+document.write(data_e);
+document.write(data_f);
+document.write(data_g);
+document.write(data_x);
