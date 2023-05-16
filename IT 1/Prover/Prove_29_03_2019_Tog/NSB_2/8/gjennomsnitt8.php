ad04f420e8b4fcd3a9ee7f207cff4357b9047b0e
Danny Thinh Tran <72601388+danny1401@users.noreply.github.com>
2023-05-16 15:07:58 +0200

    Add files via upload

---
+++ IT 1/Prover/Prove_29_03_2019_Tog/NSB_2/8/gjennomsnitt8.php
@@ -0,0 +1,85 @@
+<?php
+$passasjer = fopen('./../passasjerfil.txt', 'r'); 
+$passasjer = fread($passasjer,filesize('./../passasjerfil.txt'));
+fclose($passasjer);
+$seter = fopen('./../seterfil.txt', 'r'); 
+$seter = fread($seter,filesize('./../seterfil.txt'));
+fclose($seter);
+
+$passasjer2 = fopen('./../2/passasjerfil2.txt', 'r'); 
+$passasjer2 = fread($passasjer2,filesize('./../2/passasjerfil2.txt'));
+fclose($passasjer2);
+$seter2 = fopen('./../2/seterfil2.txt', 'r'); 
+$seter2 = fread($seter2,filesize('./../2/seterfil2.txt'));
+fclose($seter2);
+
+$passasjer3 = fopen('./../3/passasjerfil3.txt', 'r'); 
+$passasjer3 = fread($passasjer3,filesize('./../3/passasjerfil3.txt'));
+fclose($passasjer3);
+$seter3 = fopen('./../3/seterfil3.txt', 'r'); 
+$seter3 = fread($seter3,filesize('./../3/seterfil3.txt'));
+fclose($seter3);
+
+$passasjer4 = fopen('./../4/passasjerfil4.txt', 'r'); 
+$passasjer4 = fread($passasjer4,filesize('./../4/passasjerfil4.txt'));
+fclose($passasjer4);
+$seter4 = fopen('./../4/seterfil4.txt', 'r'); 
+$seter4 = fread($seter4,filesize('./../4/seterfil4.txt'));
+fclose($seter4);
+
+$passasjer5 = fopen('./../5/passasjerfil5.txt', 'r'); 
+$passasjer5 = fread($passasjer5,filesize('./../5/passasjerfil5.txt'));
+fclose($passasjer5);
+$seter5 = fopen('./../5/seterfil5.txt', 'r'); 
+$seter5 = fread($seter5,filesize('./../5/seterfil5.txt'));
+fclose($seter5);
+
+$passasjer6 = fopen('./../6/passasjerfil6.txt', 'r'); 
+$passasjer6 = fread($passasjer6,filesize('./../6/passasjerfil6.txt'));
+fclose($passasjer6);
+$seter6 = fopen('./../6/seterfil6.txt', 'r'); 
+$seter6 = fread($seter6,filesize('./../6/seterfil6.txt'));
+fclose($seter6);
+
+$passasjer7 = fopen('./../7/passasjerfil7.txt', 'r'); 
+$passasjer7 = fread($passasjer7,filesize('./../7/passasjerfil7.txt'));
+fclose($passasjer7);
+$seter7 = fopen('./../7/seterfil7.txt', 'r'); 
+$seter7 = fread($seter7,filesize('./../7/seterfil7.txt'));
+fclose($seter7);
+
+$passasjer8 = fopen('./passasjerfil8.txt', 'r'); 
+$passasjer8 = fread($passasjer8,filesize('./passasjerfil8.txt'));
+fclose($passasjer8);
+$seter8 = fopen('./seterfil8.txt', 'r'); 
+$seter8 = fread($seter8,filesize('./seterfil8.txt'));
+fclose($seter8);
+
+$test = (($passasjer + $passasjer2 + $passasjer3 + $passasjer4 + $passasjer5 + $passasjer6 + $passasjer7 + $passasjer8) / (($seter + $seter2 + $seter3 + $seter4 + $seter5 + $seter6 + $seter7 + $seter8)/100))*3.6;
+
+$eksempel = imagecreatetruecolor(100, 100);
+
+imagesavealpha($eksempel, true);
+$trans_colour = imagecolorallocatealpha($eksempel, 0, 0, 0, 127);
+imagefill($eksempel, 0, 0, $trans_colour);
+
+$blue     = imagecolorallocate($eksempel, 0, 0, 204);
+$darkblue = imagecolorallocate($eksempel, 0, 0, 153);
+$red      = imagecolorallocate($eksempel, 255, 0, 0);
+$darkred  = imagecolorallocate($eksempel, 204, 0, 0);
+
+    for ($i = 60; $i > 50; $i--) {
+        imagefilledarc($eksempel, 50, $i, 100, 50, 0, $test, $darkblue, IMG_ARC_PIE);
+        imagefilledarc($eksempel, 50, $i, 100, 50, $test, 360, $darkred,  IMG_ARC_PIE);
+    }
+
+imagefilledarc($eksempel, 50, 50, 100, 50, 0, $test, $blue, IMG_ARC_PIE);
+imagefilledarc($eksempel, 50, 50, 100, 50, $test, 360 , $red, IMG_ARC_PIE);
+
+header('Content-type: image/png');
+imagepng($eksempel);
+imagedestroy($eksempel);
+fclose($passasjer);
+fclose($seter);
+
+?>
---
+++ IT 1/Prover/Prove_29_03_2019_Tog/NSB_2/8/register8.php
@@ -0,0 +1,25 @@
+<?php
+print '
+<html>
+<head>
+<link rel="stylesheet" href="./../style/style.css" type="text/css">
+<title>Tog og passasjertall</title>
+</head>
+<body>
+<center>
+<h1 id="title2">Tog og passasjertall</h1>
+<div id="boks">
+<br>
+<h2>Informasjonen har blitt registrert</h2>
+<br>
+<a href="./../NSB_2_index1.php"><button id="knapp3">Angre alle tog-opprettelsene</button></a>
+<br><br><br><br>
+<a href="./NSB_2_index8.php"><button id="knapp3">Angre tog-opprettelsen</button></a>
+<br><br><br><br>
+<a href="./kvittering8.php"><button id="knapp3">Vis alle tog-opprettelsene</button></a>
+</div>
+</center>
+</body>
+</html>
+';	
+?>
